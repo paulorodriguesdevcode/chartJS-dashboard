3a147e55be44f2e18981d0673436585fcb7afc82
Paulo Roberto <prpaulo07@hotmail.com>
2020-04-06 19:44:49 -0300

    clean all

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -1,15 +1,14 @@
 //Monta os graficos
-$(function() {
-
+$(function () {
     var donutChartCanvas = $('#donutChart').get(0).getContext('2d')
     var donutData = {
         labels: [
-            'CC 005',
-            'CC 001',
-            'CC 002',
-            'CC 007',
-            'CC 008',
-            'CC 004',
+            'CIA 005',
+            'CIA 001',
+            'CIA 002',
+            'CIA 007',
+            'CIA 008',
+            'CIA 004',
         ],
         datasets: [{
             data: [2700, 500, 400, 600, 300, 100],
@@ -19,13 +18,13 @@ $(function() {
     var donutOptions = {
         legend: {
             position: 'top',
-            
+
         },
-            maintainAspectRatio: false,
-            responsive: true,
-        }
-        //Create pie or douhnut chart
-        // You can switch between pie and douhnut using the method below.
+        maintainAspectRatio: false,
+        responsive: true,
+    }
+    //Create pie or douhnut chart
+    // You can switch between pie and douhnut using the method below.
     var donutChart = new Chart(donutChartCanvas, {
         type: 'doughnut',
         data: donutData,
@@ -36,97 +35,29 @@ $(function() {
 
     var donutChartCanvas1 = $('#donutChart1').get(0).getContext('2d')
     var donutData1 = {
-        labels: [           
-            'Dentro do SLA',
-            'SLA Vencido',
+        labels: [
+            'Flights in time',
+            'Delayed Flights',
         ],
         datasets: [{
             data: [2309, 891],
-            backgroundColor: ['#00a65a','#f56954'],
-            
-           
+            backgroundColor: ['#00a65a', '#f56954'],
         }]
     }
-    
-
 
     var donutOptions1 = {
-            maintainAspectRatio: false,
-            responsive: true,
-            legend: {
-                position: 'top',
-                
-            },
-            
-        }
-        //Create pie or douhnut chart
-        // You can switch between pie and douhnut using the method below.
+        maintainAspectRatio: false,
+        responsive: true,
+        legend: {
+            position: 'top',
+
+        },
+
+    }
     var donutChart1 = new Chart(donutChartCanvas1, {
         type: 'doughnut',
         data: donutData1,
         options: donutOptions1,
     })
     console.log(donutChart1)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-
-
-    //-------------
-    //- BAR CHART -
-    //-------------
-    var barChartCanvas = $('#barChart').get(0).getContext('2d')
-    var barChartData = {
-        labels: ['01/10/2019', '02/10/2019', '03/10/2019', '04/10/2019', '05/10/2019', '06/10/2019', '07/10/2019', '08/10/2019', '09/10/2019', '10/10/2019', '11/10/2019', '12/10/2019', '13/10/2019', '14/10/2019', '15/10/2019'],
-        datasets: [{
-            label: 'Total $ Vendas / Dia',
-            backgroundColor: 'rgba(60,141,188,0.9)',
-            borderColor: 'rgba(60,141,188,0.8)',
-            pointRadius: false,
-            pointColor: '#3b8bba',
-            pointStrokeColor: 'rgba(60,141,188,1)',
-            pointHighlightFill: '#fff',
-            pointHighlightStroke: 'rgba(60,141,188,1)',
-            data: [2000.00, 1480.00, 900.00, 1190.00, 860.50, 1570.20, 1900.00]
-        }]
-    }
-    var temp0 = barChartData.datasets[0]
-
-    var barChartOptions = {
-        responsive: true,
-        maintainAspectRatio: false,
-        datasetFill: false
-    }
-
-    var barChart = new Chart(barChartCanvas, {
-        type: 'bar',
-        data: barChartData,
-        options: barChartOptions
-    })
 })
